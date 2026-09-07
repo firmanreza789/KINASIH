c78428dd788b9c84a005df9fe2b15f4b95c6c726
Hello_Friends789 <75846010+firmanreza789@users.noreply.github.com>
2026-09-07 15:11:41 +0700

    Delete website/backend directory

--- website/backend/prisma/migrations/20260902145543_unique_nama_kategori/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nama]` on the table `kategori_sampah` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "kategori_sampah_nama_key" ON "kategori_sampah"("nama");
